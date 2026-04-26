252346de20e4db0470e48834efc2c8b220facac4
Jiny8 <125109569+Jiny8@users.noreply.github.com>
2026-04-26 19:33:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>부산 투어</h1>
+      <p>부산 여행 투어 예약 서비스에 오신 것을 환영합니다.</p>
+    </div>
+  );
+}
+
+export default App;
