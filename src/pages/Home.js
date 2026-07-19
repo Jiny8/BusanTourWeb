ba1e7e0b0d3e08b44b0e627b58d06b0edadf36ad
Jiny8 <125109569+Jiny8@users.noreply.github.com>
2026-07-19 16:10:39 +0900

    fix: design
    배너이미지 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../styles/Home.css";
+import img1 from '../assets/images/img1.jpg'
+import img2 from '../assets/images/img2.jpg'
+import img3 from '../assets/images/img3.jpg'
+import img4 from '../assets/images/img4.jpg'
 
 const SLIDE_IMAGES = [
-  "https://visitbusan.net/uploadImgs/files/cntnts/20221115110601843_wufrotr",
-  "https://visitbusan.net/uploadImgs/files/cntnts/20221115110556317_wufrotr",
-  "https://visitbusan.net/uploadImgs/files/cntnts/20221115110556661_wufrotr",
-  "https://visitbusan.net/uploadImgs/files/cntnts/20220413144137777_wufrotr",
+  img1, img2, img3, img4
 ];
 
 export default function Home() {
